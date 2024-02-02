d6bb5d8ebac2c8996f7993291a34571cfa40fd35
Juan José Muñoz Noda <61985033+JoseCoscu@users.noreply.github.com>
2024-02-02 18:04:35 -0500

    Cargando documentos txt y agregada la estructura del repo

--- src/code/doc_loader.py
+++ src/code/doc_loader.py
@@ -3,16 +3,17 @@ def_path = "../../data/"
 
 def load_doc(name):
     # Abre el archivo en modo lectura ('r')
-    with open(name, 'r') as archivo:
+    with open(def_path + name, 'r') as archivo:
         # Lee el contenido del archivo
         doc = archivo.read()
 
     # Imprime el contenido del archivo
     return doc
 
-doc1 = load_doc(def_path + "naturaleza.txt")
+doc1 = load_doc("naturaleza.txt")
+doc2 = load_doc("naturaleza_plagio.txt")
 
-print (doc1)
+print (doc2)
 
 
 
